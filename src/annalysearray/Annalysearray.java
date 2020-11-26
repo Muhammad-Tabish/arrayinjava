fb888d586ae5fa71a4ba49e5c80d92876289d693
tabishhameed <tabishhameed34@gmail.com>
2020-11-27 00:33:46 +0800

    new array sort

--- src/annalysearray/Annalysearray.java
+++ src/annalysearray/Annalysearray.java
@@ -6,18 +6,58 @@
 package annalysearray;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  *
  * @author Muhammad Tabish
  */
 public class Annalysearray {
-
-    
-    public static void main(String[] args) {
+    public static void Annalysearray(int [] numbers){
+        int negativeNums = 0;
+        int positiveNums = 0;
+        int zeros = 0;
+        for(int i = 0; i < numbers.length; i++) {
+            
+            if (numbers[i] > 0) {
+                positiveNums++;
+            } else if (numbers[i] < 0) {
+                negativeNums++;
+            } else {
+                zeros++;
+            }
+        }
+        
+        System.out.println(" RESULT ");
+      
+        if(negativeNums == 0){
+            System.out.println("NO negatives\n");
+        }
+        else{
+            System.out.println("there are " + negativeNums + " negative numbers\n");
+        }
+        if(positiveNums == 0){
+            System.out.println("NO positives");
+        }
+        else{
+            System.out.println("there are " + positiveNums + " positive numbers\n");
+        }
+        if(zeros == 0){
+            System.out.println("NO zeros");
+        }
+        else{
+            System.out.println("there are " + zeros+ " zeros\n");
+        }
+           
+    }
+        
+   
+  public static void main(String [] args){
         
      int[] myNum = {10, -20, 30, 0,  40};
-       
+    
+     
+        
         for (int element: myNum) {
             System.out.println(element);
             
@@ -69,5 +109,14 @@ public class Annalysearray {
 Arrays.sort(myNum);
          System. out. println("Minimum = " + myNum[0]);
          System. out. println("Maximum = " + myNum[myNum. length-1]);
+    
+   
+int [] innums ={10, -20, 30, 0,  40} ;
+        Annalysearray(innums);
+        
     }
+    
 }
+
+
+
